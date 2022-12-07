cbccb0b758312a17e476d3afb84348b54f331d4b
Viacheslav Panasiuk <badgerle82@gmail.com>
2022-12-07 19:36:50 +0200

    DI realization, UseCases, States and ViewModels were added

---
+++ app/src/main/java/com/example/cryptocurrencyappmvvmcompose/CoinApplication.kt
@@ -0,0 +1,7 @@
+package com.example.cryptocurrencyappmvvmcompose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CoinApplication : Application()
